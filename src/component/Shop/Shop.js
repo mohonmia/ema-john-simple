b5aef504710a023a39d33a77050981fee12b4e05
mohonmia <mohonmia97@gmail.com>
2020-03-12 17:07:41 +0600

    final

---
+++ src/component/Shop/Shop.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import fakeData from '../../fakeData';
+import './Shop.css';
+
+const Shop = () => {
+    const fast10 = fakeData.slice(0,10);  
+    const [products, setProducts] = useState(fast10);
+    return (
+        <div className="shop-container">
+            <div className="product-container">
+                <ul>
+                    {
+                        products.map(product=> <li>{product.name}</li>)
+                    }
+                </ul>
+            </div>
+            <div className="cart-containeer">
+                <h3>This is cart</h3>
+            </div>
+
+        </div>
+    );
+};
+
+export default Shop;
