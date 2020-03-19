aff0537beca60e07324b713730105f4a59bbaf4b
mohonmia <mohonmia97@gmail.com>
2020-03-19 08:54:03 +0600

    update

--- src/component/Review/Review.js
+++ src/component/Review/Review.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { getDatabaseCart } from '../../utilities/databaseManager';
+import fakeData from '../../fakeData';
+import Reviewitem from './Reviewitem/Reviewitem';
 
 const Review = () => {
+    const [cart, setCart] = useState([]);
+    useEffect(()=>{
+        const savedCart = getDatabaseCart();
+        const productKeys = Object.keys(savedCart);
+        
+        const cartProducts = productKeys.map(key =>{
+            const product = fakeData.find(pd => pd.key === key);
+            product.quantity = savedCart[key];
+            return product;
+        });
+        setCart(cartProducts);
+    },[]);
     return (
         <div>
-            <h1>this is review page</h1>
+            <h1>cart items: {cart.length}</h1>{
+                cart.map(pd => 
+                <Reviewitem  key={pd.key} product={pd}>
+                </Reviewitem>)
+            }
         </div>
     );
 };
---
+++ src/component/Review/Reviewitem/Reviewitem.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Reviewitem = (props) => {
+    const {name,quantity} = props.product;
+    const reviewStyle = {
+        borderBottom: '1px solid lightgray',
+        marginBottom: '5px',
+        paddingBottom: '5px',
+        marginLeft: '200px' 
+    }
+    return (
+        <div style={reviewStyle} className="review-item">
+            <h4 className="product-name">{name}</h4>
+            <p>Quantity: {quantity}</p>
+            <button className="main-button">Remove</button>
+        </div>
+    );
+};
+
+export default Reviewitem;
